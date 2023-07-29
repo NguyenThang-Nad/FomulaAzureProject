f736bebd3fa14b739966ba5d573ff65ea0776eaa
NguyenThang-Nad <NguyenThang-Nad>
2023-07-29 09:23:20 +0000

    c

---
+++ circuilts_ing.py
@@ -0,0 +1,51 @@
+# Databricks notebook source
+#circuits.csv to spark dataframe
+circuits_df = spark.read \
+    .option("header", "true") \
+    .option("inferSchema", "true") \
+    .csv("/mnt/formuladatalake123/raw/circuits.csv") 
+
+# COMMAND ----------
+
+display(circuits_df)
+circuits_df.printSchema()
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col
+
+# COMMAND ----------
+
+#drop colum url
+circuits_selected_df = circuits_df.drop(col("url"))
+
+# COMMAND ----------
+
+#rename required columns
+circuits_renamed_df = circuits_selected_df.withColumnRenamed("circuitId", "circuit_id") \
+.withColumnRenamed("circuitRef", "circuit_ref") \
+.withColumnRenamed("lat", "latitude") \
+.withColumnRenamed("lng", "longitude") \
+.withColumnRenamed("alt", "altitude") 
+
+# COMMAND ----------
+
+from pyspark.sql.functions import current_timestamp
+circuits_final_df = circuits_renamed_df.withColumn("ingestion_date",current_timestamp())
+
+# COMMAND ----------
+
+display(circuits_final_df)
+
+# COMMAND ----------
+
+#save dataframe as Parquet format
+circuits_final_df.write.mode("overwrite").parquet("/mnt/formuladatalake123/processed/circuits")
+
+# COMMAND ----------
+
+display(spark.read.parquet("/mnt/formuladatalake123/processed/circuits"))
+
+# COMMAND ----------
+
+
